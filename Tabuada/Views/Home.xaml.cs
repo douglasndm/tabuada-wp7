ebf4123aa49e3b0fb590ec617b5740e32bf0502b
Douglas Nunes de Mattos <DouglasNunesdeMattos@outlook.com>
2020-12-28 20:48:40 -0300

    Creating home layout

--- Tabuada/Views/Home.xaml.cs
+++ Tabuada/Views/Home.xaml.cs
@@ -11,12 +11,11 @@ using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 using Microsoft.Phone.Controls;
 
-namespace Tabuada
+namespace Tabuada.Views
 {
-    public partial class MainPage : PhoneApplicationPage
+    public partial class Home : PhoneApplicationPage
     {
-        // Constructor
-        public MainPage()
+        public Home()
         {
             InitializeComponent();
         }
