8e8faf52d2e3b44fad3cf0ec64b9c4a56c6aaa24
Douglas Nunes de Mattos <DouglasNunesdeMattos@outlook.com>
2020-12-28 21:59:27 -0300

    Adding navigation to results page

--- Tabuada/Views/Home.xaml.cs
+++ Tabuada/Views/Home.xaml.cs
@@ -18,6 +18,32 @@ namespace Tabuada.Views
         public Home()
         {
             InitializeComponent();
+
+            var numericScope = new InputScope();
+            var numericScopeName = new InputScopeName();
+            numericScopeName.NameValue = InputScopeNameValue.Number;
+            numericScope.Names.Add(numericScopeName);
+            TextBox_NumCalc.InputScope = numericScope;
+            TextBox_NumHowManyTimes.InputScope = numericScope; 
+        }
+
+        private void Btn_calc_Click(object sender, RoutedEventArgs e)
+        {
+            int numCalc, numHowManyTimes;
+
+            try
+            {
+                numCalc = System.Convert.ToInt32(TextBox_NumCalc.Text);
+                numHowManyTimes = System.Convert.ToInt32(TextBox_NumHowManyTimes.Text);
+
+                NavigationService.Navigate(
+                    new Uri(String.Format("/Views/Results.xaml?numCalc={0}&numHowManyTimes={1}", numCalc, numHowManyTimes), 
+                            UriKind.RelativeOrAbsolute));
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.Message);
+            }
         }
     }
 }
---
+++ Tabuada/Views/Results.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+
+namespace Tabuada.Views
+{
+    public partial class Results : PhoneApplicationPage
+    {
+        int numCalc, numHowManyTimes;
+        public Results()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(System.Windows.Navigation.NavigationEventArgs e)
+        {
+            IDictionary<string, string> parameters = NavigationContext.QueryString;
+
+            if (parameters.ContainsKey("numCalc") && parameters.ContainsKey("numHowManyTimes"))
+            {
+                try
+                {
+                    numCalc = System.Convert.ToInt32(parameters["numCalc"]);
+                    numHowManyTimes = System.Convert.ToInt32(parameters["numHowManyTimes"]);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            else
+            {
+                throw new Exception("Results page didn't get right datas");
+            }
+
+            base.OnNavigatedTo(e);
+        }
+    }
+}
